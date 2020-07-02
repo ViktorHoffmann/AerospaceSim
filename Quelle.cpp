12c9a69bb5806bdd8a3940060ae40ed8cf713836
Viktor Hoffmann <viktor.m.hoffmann@gmail.com>
2020-07-02 12:33:41 +0200

    csv read function

--- Quelle.cpp
+++ Quelle.cpp
@@ -141,33 +141,20 @@ double dyn_pres_model(double vel, double alt) {
 void read_csv(std::string Input_file) {
 	std::stringstream ifile(Input_file);
 	std::string line;
-	while (std::getline(ifile, line)) // read the current line
-	{
-		std::istringstream iss{ line }; // construct a string stream from line
 
-		// read the tokens from current line separated by semiiicoLOOOON
-		std::vector<std::string> tokens; // here we store the tokens
-		std::string token; // current token
+	while (std::getline(ifile, line))
+	{
+		std::istringstream iss{ line };
+		std::vector<std::string> tokens;
+		std::string token;
+
 		while (std::getline(iss, token, ';'))
 		{
-			tokens.push_back(token); // add the token to the vector
+			tokens.push_back(token);
 		}
 
-		// we can now process the tokens
-		// first display them
-		std::cout << "Tokenized line: ";
-		for (const auto& elem : tokens)
-			std::cout << "[" << elem << "]";
-		std::cout << std::endl;
-
-		// map the tokens into our variables, this applies to your scenario
-		std::string name = tokens[0]; // first is a string, no need for further processing
-		int age = std::stoi(tokens[1]); // second is an int, convert it
-		int height = std::stoi(tokens[2]); // same for third
-		std::cout << "Processed tokens: " << std::endl;
-		std::cout << "\t Name: " << name << std::endl;
-		std::cout << "\t Age: " << age << std::endl;
-		std::cout << "\t Height: " << height << std::endl;
+		int altitude = std::stoi(tokens[0]);
+		int velocity = std::stoi(tokens[1]);
 	}
 }
 
